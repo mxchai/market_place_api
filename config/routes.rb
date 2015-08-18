3ed9f0264fb3134d1e7cbe464e045f4f1fb46744
MX Chai <mingxuan@live.com.sg>
2015-08-18 18:38:06 +0800

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  
+  # API definition
+  # By defining a namespace under the routes.rb file, 
+  # Rails will automatically map that namespace to a directory **matching**
+  # the name under the **controllers** folder, in our case the api/ directory.
+
+  # the constraints thing is to make api.example.com instead of example.com/api
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/' do
+
+
+  end
+
 end
